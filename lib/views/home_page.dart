aa6f75039a6c4cbeacf7c824d704c77f55c64f93
Tryo Asnafi <tryoasnafi@gmail.com>
2021-06-28 09:35:17 +0700

    Add list view for news

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 
+import '../models/news.dart';
+import '../services/api_service.dart';
+import '../widgets/customListTile.dart';
+
 class HomePage extends StatefulWidget {
-  const HomePage({Key key}) : super(key: key);
+  final News news;
+
+  const HomePage({Key key, this.news}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  Future<List<News>> news;
+
+  @override
+  void initState() {
+    super.initState();
+    news = ApiService().fetchNews();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +29,25 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         title: Text("Tryo Asnafi"),
       ),
-      body: Container(),
+      body: FutureBuilder(
+        future: news,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<News> listNews = snapshot.data;
+            return ListView.builder(
+              itemCount: listNews.length,
+              physics: BouncingScrollPhysics(),
+              itemBuilder: (context, index) => customListTile(listNews[index]),
+            );
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
     );
   }
 }
