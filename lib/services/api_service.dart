7a28d916e47e94cc945a8b609c7e8cf7f81e6e9c
Tryo Asnafi <tryoasnafi@gmail.com>
2021-06-28 08:58:55 +0700

    Add API service for fetch data

---
+++ lib/services/api_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import '../models/news.dart';
+
+class ApiService {
+  final endPointURL = "https://jaroji.web.id/api/news.php";
+
+  Future<List<News>> fetchNews() async {
+    final response = await get(Uri.tryParse(endPointURL));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> json = jsonDecode(response.body);
+
+      List<dynamic> body = json['news'];
+
+      List<News> news = body.map((item) => News.fromJSON(item)).toList();
+
+      return news;
+    } else {
+      throw Exception("Can't get the News!");
+    }
+  }
+
+}
